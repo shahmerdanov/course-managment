5cb71d5d7fe037a23b27d7ed6e5a518198e5261f
ShamilSh777 <105242215+ShamilSh777@users.noreply.github.com>
2023-12-26 17:14:40 +0400

    a few changing in service class

--- src/main/java/az/englishschool/englishschool/service/impl/StudentsServiceImpl.java
+++ src/main/java/az/englishschool/englishschool/service/impl/StudentsServiceImpl.java
@@ -23,26 +23,25 @@ public class StudentsServiceImpl implements StudentsService {
     public StudentResponse getAllStudents() {
         List<StudentDto> studentsDTOList = studentsRepository.findAll()
                 .stream()
-                .map(students -> convertToDto(students))
+                .map(this::convertToDto)
                 .collect(Collectors.toList());
         return makeStudentResponse(studentsDTOList);
-
     }
 
     @Override
     public StudentDto getStudent(long id) {
         return studentsRepository.findById(id)
-                .map(students -> convertToDto(students))
+                .map(this::convertToDto)
                 .orElseThrow(() -> new StudentRestException("No such student found"));
     }
 
     @Override
     public StudentResponse getStudentByNameAndSurname(String name, String surname) {
-        List<StudentDto> studentss = studentsRepository.findByNameAndSurname(name, surname)
+        List<StudentDto> students = studentsRepository.findByNameAndSurname(name, surname)
                 .stream()
-                .map(students -> convertToDto(students))
+                .map(this::convertToDto)
                 .collect(Collectors.toList());
-        return makeStudentResponse(studentss);
+        return makeStudentResponse(students);
     }
 
     @Override
@@ -55,11 +54,7 @@ public class StudentsServiceImpl implements StudentsService {
     @Override
     public void updateStudents(StudentDto studentsDTO, long id) {
         Students students = getStudentById(id);
-
-        students.setName(studentsDTO.getName());
-        students.setSurname(studentsDTO.getSurname());
-        students.setAge(studentsDTO.getAge());
-        students.setGroupName(students.getGroupName());
+        BeanUtils.copyProperties(studentsDTO, students);
         studentsRepository.save(students);
     }
 
@@ -67,17 +62,10 @@ public class StudentsServiceImpl implements StudentsService {
     public void updateSomeStudents(StudentDto studentsDTO, long id) {
         Students students = getStudentById(id);
 
-        if (studentsDTO.getName()!=null)
-        students.setName(studentsDTO.getName());
-
-        if (studentsDTO.getSurname()!=null)
-        students.setSurname(studentsDTO.getSurname());
-
-        if (studentsDTO.getAge()>0)
-        students.setAge(studentsDTO.getAge());
-
-        if (studentsDTO.getGroupName()!=null)
-        students.setGroupName(students.getGroupName());
+        if (studentsDTO.getName() != null) students.setName(studentsDTO.getName());
+        if (studentsDTO.getSurname() != null) students.setSurname(studentsDTO.getSurname());
+        if (studentsDTO.getAge() > 0) students.setAge(studentsDTO.getAge());
+        if (studentsDTO.getGroupName() != null) students.setGroupName(studentsDTO.getGroupName());
 
         studentsRepository.save(students);
     }
@@ -104,5 +92,4 @@ public class StudentsServiceImpl implements StudentsService {
                 .studentsDTOS(students)
                 .build();
     }
-
 }
